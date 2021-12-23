731953c3081d17ccd0c3a7faab0d2a10ab703ab2
Harish5513 <96466381+Harish5513@users.noreply.github.com>
2021-12-23 16:00:49 +0530

    Add files via upload

---
+++ dict_value_access_7.py
@@ -0,0 +1,12 @@
+dict = { "class" :}
+Dict = {
+"class":{
+"student":{
+"name":"Mike",
+"marks":{
+"physics":70,
+"history":80
+}
+}
+}
+}
---
+++ reverse_list_1.py
@@ -0,0 +1,5 @@
+def reverse():
+    list1.sort(reverse=True)
+    print(list1)
+list1 = [100, 200, 300, 400, 500]
+reverse()
